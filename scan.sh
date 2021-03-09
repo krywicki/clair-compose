370257074005a77e96c786fb47e779eb609be956
Joe Krywicki <joe.krywicki@topbox.io>
2021-03-09 16:31:09 -0600

    Fixed misspelled executable names

--- scan.sh
+++ scan.sh
@@ -8,13 +8,13 @@ IMAGE=$1
 if [[ -f "clair-scanner-mac" ]]; then
     IP_ADDR=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
     docker-compose up -d
-    ./clair-scanner --ip $IP_ADDR $IMAGE
+    ./clair-scanner-mac --ip $IP_ADDR $IMAGE
     docker-compose down
 
 # Run on Linux
 elif [[ -f "clair-scanner-linux" ]]; then
     docker-compose up -d
-    ./clair-scanner $IMAGE
+    ./clair-scanner-linux $IMAGE
     docker-compose down
 
 # Could not identify distribution
